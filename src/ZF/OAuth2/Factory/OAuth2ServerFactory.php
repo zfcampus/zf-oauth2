9742f4835e4a814687b3d967314e2e0b4d0b2935
Matthew Weier O'Phinney <matthew@zend.com>
2014-03-17 09:10:51 -0500

    Formatting changes
    - Mainly readability changes
    - Also changed 'Configuration' to 'Config' when fetching service

--- src/ZF/OAuth2/Factory/OAuth2ServerFactory.php
+++ src/ZF/OAuth2/Factory/OAuth2ServerFactory.php
@@ -20,11 +20,11 @@ class OAuth2ServerFactory implements FactoryInterface
     /**
      * @param ServiceLocatorInterface $services
      * @return OAuth2\Server
-     * @throws \ZF\OAuth2\Controller\Exception\RuntimeException
+     * @throws Exception\RuntimeException
      */
     public function createService(ServiceLocatorInterface $services)
     {
-        $config   = $services->get('Configuration');
+        $config   = $services->get('Config');
 
         if (!isset($config['zf-oauth2']['storage']) || empty($config['zf-oauth2']['storage'])) {
             throw new Exception\RuntimeException(
@@ -34,12 +34,16 @@ class OAuth2ServerFactory implements FactoryInterface
 
         $storage = $services->get($config['zf-oauth2']['storage']);
 
-        $enforceState   = isset($config['zf-oauth2']['enforce_state'])  ? $config['zf-oauth2']['enforce_state']  : true;
-        $allowImplicit  = isset($config['zf-oauth2']['allow_implicit']) ? $config['zf-oauth2']['allow_implicit'] : false;
+        $enforceState   = isset($config['zf-oauth2']['enforce_state'])   ? $config['zf-oauth2']['enforce_state']   : true;
+        $allowImplicit  = isset($config['zf-oauth2']['allow_implicit'])  ? $config['zf-oauth2']['allow_implicit']  : false;
         $accessLifetime = isset($config['zf-oauth2']['access_lifetime']) ? $config['zf-oauth2']['access_lifetime'] : 3600;
 
         // Pass a storage object or array of storage objects to the OAuth2 server class
-        $server = new OAuth2Server($storage, array('enforce_state' => $enforceState, 'allow_implicit' => $allowImplicit, 'access_lifetime' => $accessLifetime));
+        $server = new OAuth2Server($storage, array(
+            'enforce_state'   => $enforceState,
+            'allow_implicit'  => $allowImplicit,
+            'access_lifetime' => $accessLifetime
+        ));
 
         // Add the "Client Credentials" grant type (it is the simplest of the grant types)
         $server->addGrantType(new ClientCredentials($storage));
