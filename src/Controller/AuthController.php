ab415c6d756fd5d487bab5b544530491611b7a06
Daniel Burgess <daniel.b.burgess@gmail.com>
2015-03-05 14:11:04 -0500

    Fixes #66, a bug with null bodyParams not being handled.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -216,7 +216,7 @@ class AuthController extends AbstractActionController
 
         return new OAuth2Request(
             $zf2Request->getQuery()->toArray(),
-            $this->bodyParams(),
+            $bodyParams,
             array(), // attributes
             array(), // cookies
             array(), // files
