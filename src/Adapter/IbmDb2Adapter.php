90c74de241dad8fb1c96feca8b947c13d31c389b
Matthew Weier O'Phinney <matthew@zend.com>
2015-04-15 11:30:47 -0500

    Reviewed #89
    - Changed snake_case to camelCase variable names.
    - Do not pass-by-reference; return the updated value and re-assign
      instead.
    - Import and throw appropriate exceptions.

--- src/Adapter/IbmDb2Adapter.php
+++ src/Adapter/IbmDb2Adapter.php
@@ -1,13 +1,19 @@
 <?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
 
 namespace ZF\OAuth2\Adapter;
 
 use OAuth2\Storage\IbmDb2 as OAuth2Db2;
+use RuntimeException;
 use Zend\Crypt\Password\Bcrypt;
 
 /**
  * Extension of OAuth2\Storage\IbmDb2 that provides Bcrypt client_secret/password
  * encryption
+ *
  * @author Alan Seiden <alan at alanseiden dot com> (of IbmDb2 changes)
  */
 class IbmDb2Adapter extends OAuth2Db2
@@ -21,6 +27,19 @@ class IbmDb2Adapter extends OAuth2Db2
      * @var Bcrypt
      */
     protected $bcrypt;
+
+    /**
+     * @param string $connection
+     * @param array $config
+     */
+    public function __construct($connection, $config = array())
+    {
+        parent::__construct($connection, $config);
+
+        if (isset($config['bcrypt_cost'])) {
+            $this->setBcryptCost($config['bcrypt_cost']);
+        }
+    }
 
     /**
      * @return Bcrypt
@@ -58,18 +77,19 @@ class IbmDb2Adapter extends OAuth2Db2
     }
 
     /**
-     * @param $string
+     * @param string $string
+     * @return string
      */
-    protected function createBcryptHash(&$string)
+    protected function createBcryptHash($string)
     {
-        $string = $this->getBcrypt()->create($string);
+        return $this->getBcrypt()->create($string);
     }
 
     /**
      * Check hash using bcrypt
      *
-     * @param $hash
-     * @param $check
+     * @param string $hash
+     * @param string $check
      * @return bool
      */
     protected function verifyHash($check, $hash)
@@ -78,25 +98,13 @@ class IbmDb2Adapter extends OAuth2Db2
     }
 
     /**
-     * @param string $connection
-     * @param array $config
-     */
-    public function __construct($connection, $config = array())
-    {
-        parent::__construct($connection, $config);
-        if (isset($config['bcrypt_cost'])) {
-            $this->setBcryptCost($config['bcrypt_cost']);
-        }
-    }
-
-    /**
      * Check client credentials
      *
-     * @param string $client_id
-     * @param string $client_secret
+     * @param string $clientId
+     * @param null|string $clientSecret
      * @return bool
      */
-    public function checkClientCredentials($client_id, $client_secret = null)
+    public function checkClientCredentials($clientId, $clientSecret = null)
     {
         $stmt = db2_prepare($this->db, sprintf(
             'SELECT * from %s where client_id = ?',
@@ -106,44 +114,44 @@ class IbmDb2Adapter extends OAuth2Db2
             throw new \Exception(db2_stmt_errormsg());
         }
 
-        $successfulExecute = db2_execute($stmt, compact('client_id'));
+        $successfulExecute = db2_execute($stmt, compact('clientId'));
         $result = db2_fetch_assoc($stmt);
 
         // bcrypt verify
-        return $this->verifyHash($client_secret, $result['client_secret']);
+        return $this->verifyHash($clientSecret, $result['client_secret']);
     }
 
     /**
      * Set client details
      *
-     * @param string $client_id
-     * @param string $client_secret
-     * @param string $redirect_uri
-     * @param string $grant_types
-     * @param string $scope_or_user_id If 5 arguments, user_id; if 6, scope.
-     * @param string $user_id
+     * @param string $clientId
+     * @param null|string $clientSecret
+     * @param null|string $redirectUri
+     * @param null|string $grantTypes
+     * @param null|string $scopeOrUserId If 5 arguments, userId; if 6, scope.
+     * @param null|string $userId
      * @return bool
      */
     public function setClientDetails(
-        $client_id,
-        $client_secret = null,
-        $redirect_uri = null,
-        $grant_types = null,
-        $scope_or_user_id = null,
-        $user_id = null
+        $clientId,
+        $clientSecret = null,
+        $redirectUri = null,
+        $grantTypes = null,
+        $scopeOrUserId = null,
+        $userId = null
     ) {
         if (func_num_args() > 5) {
-            $scope = $scope_or_user_id;
+            $scope = $scopeOrUserId;
         } else {
-            $user_id = $scope_or_user_id;
-            $scope   = null;
+            $userId = $scopeOrUserId;
+            $scope  = null;
         }
 
-        if (!empty($client_secret)) {
-            $this->createBcryptHash($client_secret);
+        if (! empty($clientSecret)) {
+            $clientSecret = $this->createBcryptHash($clientSecret);
         }
         // if it exists, update it.
-        if ($this->getClientDetails($client_id)) {
+        if ($this->getClientDetails($clientId)) {
             $stmt = db2_prepare($this->db, sprintf(
                 'UPDATE %s '
                 . 'SET '
@@ -155,7 +163,7 @@ class IbmDb2Adapter extends OAuth2Db2
                 . 'WHERE client_id=?',
                 $this->config['client_table']
             ));
-            $params = compact('client_secret', 'redirect_uri', 'grant_types', 'scope', 'user_id', 'client_id');
+            $params = compact('clientSecret', 'redirectUri', 'grantTypes', 'scope', 'userId', 'clientId');
 
         } else {
             $stmt = db2_prepare($this->db, sprintf(
@@ -163,11 +171,11 @@ class IbmDb2Adapter extends OAuth2Db2
                 . 'VALUES (?, ?, ?, ?, ?, ?)',
                 $this->config['client_table']
             ));
-            $params = compact('client_id', 'client_secret', 'redirect_uri', 'grant_types', 'scope', 'user_id');
+            $params = compact('clientId', 'clientSecret', 'redirectUri', 'grantTypes', 'scope', 'userId');
 
         }
-        if (false == $stmt) {
-            throw new \Exception(db2_stmt_errormsg());
+        if (false === $stmt) {
+            throw new RuntimeException(db2_stmt_errormsg());
         }
         return db2_execute($stmt, $params);
     }
@@ -184,7 +192,7 @@ class IbmDb2Adapter extends OAuth2Db2
     public function setUser($username, $password, $firstName = null, $lastName = null)
     {
         // do not store in plaintext, use bcrypt
-        $this->createBcryptHash($password);
+        $password = $this->createBcryptHash($password);
 
         // if it exists, update it.
         if ($this->getUser($username)) {
@@ -194,15 +202,15 @@ class IbmDb2Adapter extends OAuth2Db2
             ));
             $params = compact('password', 'firstName', 'lastName', 'username');
         } else {
-            $stmt = db2_prepare($this->db,sprintf(
+            $stmt = db2_prepare($this->db, sprintf(
                 'INSERT INTO %s (username, password, first_name, last_name) '
                 . 'VALUES (?, ?, ?, ?)',
                 $this->config['user_table']
             ));
             $params = compact('username', 'password', 'firstName', 'lastName');
         }
-        if (false == $stmt) {
-            throw new \Exception(db2_stmt_errormsg());
+        if (false === $stmt) {
+            throw new RuntimeException(db2_stmt_errormsg());
         }
 
         return db2_execute($stmt, $params);
--- src/Factory/IbmDb2AdapterFactory.php
+++ src/Factory/IbmDb2AdapterFactory.php
@@ -15,33 +15,41 @@ class IbmDb2AdapterFactory implements FactoryInterface
 {
     /**
      * @param ServiceLocatorInterface $services
-     * @throws \ZF\OAuth2\Controller\Exception\RuntimeException
-     * @return \ZF\OAuth2\Adapter\IbmDb2Adapter
+     * @return IbmDb2Adapter
+     * @throws Exception\RuntimeException
      */
     public function createService(ServiceLocatorInterface $services)
     {
         $config = $services->get('Config');
 
-        if (!isset($config['zf-oauth2']['db']) || empty($config['zf-oauth2']['db'])) {
+        if (! isset($config['zf-oauth2']['db']) || empty($config['zf-oauth2']['db'])) {
             throw new Exception\RuntimeException(
                 'The database configuration [\'zf-oauth2\'][\'db\'] for OAuth2 is missing'
             );
         }
 
-        $username = isset($config['zf-oauth2']['db']['username']) ? $config['zf-oauth2']['db']['username'] : null;
-        $password = isset($config['zf-oauth2']['db']['password']) ? $config['zf-oauth2']['db']['password'] : null;
-        $driver_options  = isset($config['zf-oauth2']['db']['driver_options']) ? $config['zf-oauth2']['db']['driver_options'] : array();
+        $username = isset($config['zf-oauth2']['db']['username'])
+            ? $config['zf-oauth2']['db']['username']
+            : null;
+        $password = isset($config['zf-oauth2']['db']['password'])
+            ? $config['zf-oauth2']['db']['password']
+            : null;
+        $driver_options  = isset($config['zf-oauth2']['db']['driver_options'])
+            ? $config['zf-oauth2']['db']['driver_options']
+            : array();
 
         $oauth2ServerConfig = array();
-        if (isset($config['zf-oauth2']['storage_settings']) && is_array($config['zf-oauth2']['storage_settings'])) {
+        if (isset($config['zf-oauth2']['storage_settings'])
+            && is_array($config['zf-oauth2']['storage_settings'])
+        ) {
             $oauth2ServerConfig = $config['zf-oauth2']['storage_settings'];
         }
 
         return new IbmDb2Adapter(array(
-            'database'      => $config['zf-oauth2']['db']['database'],
-            'username' => $username,
-            'password' => $password,
-            'driver_options'  => $driver_options,
+            'database'       => $config['zf-oauth2']['db']['database'],
+            'username'       => $username,
+            'password'       => $password,
+            'driver_options' => $driver_options,
         ), $oauth2ServerConfig);
     }
 }
